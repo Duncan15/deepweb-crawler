433e7268e2ddfa455b9fe61ceb5eb820abfc82b5
cwc <cwcing@gmail.com>
2019-01-02 23:43:07 +0800

    保存数据库备份

---
+++ webcrawler.sql
@@ -0,0 +1,156 @@
+-- MySQL dump 10.13  Distrib 8.0.13, for osx10.12 (x86_64)
+--
+-- Host: localhost    Database: webcrawler
+-- ------------------------------------------------------
+-- Server version	8.0.13
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+ SET NAMES utf8mb4 ;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `current`
+--
+
+DROP TABLE IF EXISTS `current`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+ SET character_set_client = utf8mb4 ;
+CREATE TABLE `current` (
+  `webId` int(20) unsigned NOT NULL,
+  `round` varchar(256) NOT NULL DEFAULT '0',
+  `M1status` varchar(256) NOT NULL DEFAULT 'inactive',
+  `M2status` varchar(256) NOT NULL DEFAULT 'inactive',
+  `M3status` varchar(256) NOT NULL DEFAULT 'inactive',
+  `M4status` varchar(256) NOT NULL DEFAULT 'inactive',
+  `SampleData_sum` int(11) NOT NULL DEFAULT '0',
+  KEY `round` (`round`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `current`
+--
+
+LOCK TABLES `current` WRITE;
+/*!40000 ALTER TABLE `current` DISABLE KEYS */;
+/*!40000 ALTER TABLE `current` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `pattern`
+--
+
+DROP TABLE IF EXISTS `pattern`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+ SET character_set_client = utf8mb4 ;
+CREATE TABLE `pattern` (
+  `webId` int(20) unsigned NOT NULL DEFAULT '0',
+  `patternName` varchar(255) NOT NULL DEFAULT 'fulltext',
+  `xpath` varchar(1024) NOT NULL DEFAULT '',
+  `id` int(20) unsigned NOT NULL AUTO_INCREMENT,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=26 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `pattern`
+--
+
+LOCK TABLES `pattern` WRITE;
+/*!40000 ALTER TABLE `pattern` DISABLE KEYS */;
+INSERT INTO `pattern` VALUES (97,'fulltext','//body',11);
+/*!40000 ALTER TABLE `pattern` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `status`
+--
+
+DROP TABLE IF EXISTS `status`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+ SET character_set_client = utf8mb4 ;
+CREATE TABLE `status` (
+  `webId` int(20) unsigned NOT NULL,
+  `statusId` int(20) unsigned NOT NULL AUTO_INCREMENT,
+  `round` varchar(256) NOT NULL DEFAULT '0',
+  `type` varchar(24) NOT NULL DEFAULT 'query',
+  `fLinkNum` int(11) unsigned NOT NULL DEFAULT '0',
+  `sLinkNum` int(11) unsigned NOT NULL DEFAULT '0',
+  KEY `round` (`round`),
+  KEY `statusId` (`statusId`)
+) ENGINE=InnoDB AUTO_INCREMENT=5186 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `status`
+--
+
+LOCK TABLES `status` WRITE;
+/*!40000 ALTER TABLE `status` DISABLE KEYS */;
+/*!40000 ALTER TABLE `status` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `website`
+--
+
+DROP TABLE IF EXISTS `website`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+ SET character_set_client = utf8mb4 ;
+CREATE TABLE `website` (
+  `webId` int(20) unsigned NOT NULL AUTO_INCREMENT,
+  `webName` varchar(256) NOT NULL DEFAULT '',
+  `indexUrl` varchar(1024) NOT NULL DEFAULT '',
+  `prefix` varchar(1024) NOT NULL DEFAULT '',
+  `paramQuery` varchar(256) NOT NULL DEFAULT '',
+  `paramPage` varchar(256) NOT NULL DEFAULT '',
+  `startPageNum` varchar(256) NOT NULL DEFAULT '',
+  `paramList` varchar(1024) NOT NULL DEFAULT '',
+  `paramValueList` varchar(1024) DEFAULT '',
+  `runningMode` varchar(256) NOT NULL DEFAULT '',
+  `threadNum` int(11) NOT NULL DEFAULT '5',
+  `timeout` int(11) NOT NULL DEFAULT '3000',
+  `charset` varchar(256) NOT NULL DEFAULT 'utf8',
+  `workFile` varchar(1024) NOT NULL DEFAULT '',
+  `userParam` varchar(256) DEFAULT NULL,
+  `pwdParam` varchar(256) DEFAULT NULL,
+  `userName` varchar(256) DEFAULT NULL,
+  `password` varchar(256) DEFAULT NULL,
+  `loginUrl` varchar(1024) DEFAULT NULL,
+  `databaseSize` int(11) NOT NULL DEFAULT '0',
+  `driver` tinyint(4) NOT NULL DEFAULT '0',
+  `usable` tinyint(4) NOT NULL DEFAULT '0',
+  `createtime` varchar(256) NOT NULL DEFAULT '',
+  `creator` varchar(256) NOT NULL DEFAULT '' COMMENT 'task creator',
+  `submitXpath` varchar(256) DEFAULT NULL,
+  PRIMARY KEY (`webId`)
+) ENGINE=InnoDB AUTO_INCREMENT=109 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `website`
+--
+
+LOCK TABLES `website` WRITE;
+/*!40000 ALTER TABLE `website` DISABLE KEYS */;
+INSERT INTO `website` VALUES (97,'搜狗Test爬取','http://121.194.104.120:8080/SogouT/Search.jsp','http://121.194.104.120:8080/SogouT/Search?','keyword','curpage','1,1','Submit','Search','unstructed',20,2000,'UTF-8','F:/crawler/sougou/',NULL,NULL,NULL,NULL,NULL,0,0,0,'2018-09-08 13:25:45','',NULL);
+/*!40000 ALTER TABLE `website` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2019-01-02 23:41:59
