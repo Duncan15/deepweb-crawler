1c1c62f820f01e9dbf5ebf077486e26ba75cb3a7
cwc <cwcing@gmail.com>
2019-04-15 19:07:36 +0800

    下调set-covering下界至0.002

--- src/main/java/com/cufe/deepweb/crawler/Launcher.java
+++ src/main/java/com/cufe/deepweb/crawler/Launcher.java
@@ -318,7 +318,7 @@ public final class Launcher {
                 .build();
 
         //initialize the strategy algorithm, this algorithm would only be used in scheduler thread
-        alg = new LinearIncrementalAlgorithm.Builder(indexClient, dedu).setProductPath(Paths.get(Constant.webSite.getWorkFile(), Constant.DATA_ADDR)).setLowBound(0.02).setUpBound(0.05).build();
+        alg = new LinearIncrementalAlgorithm.Builder(indexClient, dedu).setProductPath(Paths.get(Constant.webSite.getWorkFile(), Constant.DATA_ADDR)).setLowBound(0.002).setUpBound(0.05).build();
 
         //after initialize all the utility, set the current pid to db
         try (Connection conn = sql2o.open()) {
